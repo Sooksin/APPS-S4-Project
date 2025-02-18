6df47d87c21603dc3e605685221b6fd4779048ac
Teerachai <t.sooksin@student.han.nl>
2025-02-18 13:21:04 +0100

    added display blinkie and alarm function

--- include/display.h
+++ include/display.h
@@ -1,3 +1,46 @@
+/*
+
+bool adjustingHour = true;
+bool adjustingMinute = false;
+bool blinkHour = false;
+bool blinkMinute = false;
+unsigned long lastBlinkTime = 0;
+const int blinkInterval = 500; // Blink every 500ms
+bool alarmActive = true; // Set this to true if alarm is enabled
+
+Adafruit_ST7735 tft(TFT_CS, TFT_DC, TFT_RST);
+Display display(tft);
+
+void displayTime() {
+    struct tm timeinfo;
+    if (!getLocalTime(&timeinfo)) {
+        Serial.println("Failed to obtain time");
+        return;
+    }
+
+    char timeStr[10];
+    strftime(timeStr, sizeof(timeStr), "%H:%M", &timeinfo);
+    
+    if (adjustingHour) {
+        if (millis() - lastBlinkTime > blinkInterval) {
+            blinkHour = !blinkHour; // Toggle blink state for hour
+            lastBlinkTime = millis();
+        }
+    }
+     if (adjustingMinute) {
+        if (millis() - lastBlinkTime > blinkInterval) {
+            blinkMinute = !blinkMinute; // Toggle blink state for minute
+            lastBlinkTime = millis();
+        }
+    }
+
+    display.showTimeBlink(timeStr, blinkHour, blinkMinute, alarmActive);
+    Serial.print("Current Time: ");
+    Serial.println(timeStr);
+}
+
+*/
+
 #ifndef DISPLAY_H
 #define DISPLAY_H
 
@@ -8,17 +51,18 @@
 #define TFT_CS     5   // Chip Select
 #define TFT_RST    4   // Reset
 #define TFT_DC     2   // Data/Command
-#define TFT_MOSI  23   // SPI MOSI
-#define TFT_SCLK  18   // SPI Clock
 
 class Display {
 public:
-    Display();
+    Display(Adafruit_ST7735& tft); // Dependency Injection
     void begin();
     void showTime(const char* timeStr);
+    void showTimeBlink(const char* timeStr, bool blinkHour, bool blinkMinute, bool alarmActive);
+    void clearScreen();
 
 private:
-    Adafruit_ST7735 tft;
+    Adafruit_ST7735& tft; // Reference to the display object
+    void drawAlarmIcon(bool alarmActive);
 };
 
 #endif
--- src/display.cpp
+++ src/display.cpp
@@ -1,6 +1,6 @@
 #include "Display.h"
 
-Display::Display() : tft(TFT_CS, TFT_DC, TFT_RST) {}
+Display::Display(Adafruit_ST7735& tftRef) : tft(tftRef) {}
 
 void Display::begin() {
     tft.initR(INITR_144GREENTAB);
@@ -8,10 +8,44 @@ void Display::begin() {
     tft.setRotation(1);
 }
 
+void Display::clearScreen() {
+    tft.fillScreen(ST77XX_BLACK);
+}
+
 void Display::showTime(const char* timeStr) {
-    tft.fillRect(20, 50, 100, 21, ST77XX_BLACK);
+    tft.fillRect(5, 50, 150, 30, ST77XX_BLACK); // Clear previous time
     tft.setTextColor(ST77XX_WHITE);
-    tft.setTextSize(3);
-    tft.setCursor(20, 50);
+    tft.setTextSize(4);
+    tft.setCursor(5, 50);
     tft.println(timeStr);
+}
+
+void Display::showTimeBlink(const char* timeStr, bool blinkHour, bool blinkMinute, bool alarmActive) {
+    char displayStr[10];
+    strncpy(displayStr, timeStr, sizeof(displayStr));
+    
+    if (blinkHour) {
+        displayStr[0] = ' ';
+        displayStr[1] = ' ';
+    }
+    if (blinkMinute) {
+        displayStr[3] = ' ';
+        displayStr[4] = ' ';
+    }
+
+    showTime(displayStr);
+    drawAlarmIcon(alarmActive);
+}
+
+void Display::drawAlarmIcon(bool alarmActive) {
+    if (alarmActive) {
+        tft.drawCircle(10, 10, 6, ST77XX_WHITE);
+        tft.drawCircle(10, 10, 5, ST77XX_WHITE);
+        tft.drawLine(10, 10, 10, 6, ST77XX_WHITE);
+        tft.drawLine(10, 10, 14, 10, ST77XX_WHITE);
+        tft.fillTriangle(4, 4, 6, 2, 8, 4, ST77XX_WHITE);
+        tft.fillTriangle(12, 4, 14, 2, 16, 4, ST77XX_WHITE);
+    } else {
+        tft.fillRect(2, 2, 16, 16, ST77XX_BLACK); // Clear previous alarm icon
+    }
 }
